cce2c493f8e1297fe483385dc14a5facf217e5b3
EduardoAraujoB <ea0123450@gmail.com>
2020-05-18 16:01:47 -0300

    feat: listing repos from a user

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import getAllUsers from './services/getAllUsers';
+import getUserDetails from './services/getUserDetails';
+import getUserRepos from './services/getUserRepos';
+
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json('hello');
-});
+routes.get('/users', getAllUsers);
+routes.get('/users/:username/details', getUserDetails);
+routes.get('/users/:username/repos', getUserRepos);
 
 export default routes;
