5713eb5b8824f4866fd711a412a3c053a3df24f1
EduardoAraujoB <ea0123450@gmail.com>
2020-05-18 14:27:44 -0300

    feat: created a typescript project structure

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import './bootstrap';
+import express from 'express';
+import cors from 'cors';
+
+import routes from './routes';
+
+class App {
+  public express: express.Application;
+
+  public constructor() {
+    this.express = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  public middlewares(): void {
+    this.express.use(express.json());
+    this.express.use(cors());
+  }
+
+  public routes(): void {
+    this.express.use(routes);
+  }
+}
+
+export default new App().express;
